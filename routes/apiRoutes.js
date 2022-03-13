7a9168bd5bf6a39232ca20bc6561fc990f70338b
itzguled <itzguled55337@gmail.com>
2022-03-13 15:18:23 -0500

    fixed post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
-let db = require("../db/db.json");
 let saveNote = [];
+let db = require("../db/db.json");
 
 router.get("/notes", (req, res) => {
-  const info = db;
-  res.json(info);
+  fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+    res.json(JSON.parse(data));
+  });
 });
 
 router.get("/notes/:id", (req, res) => {
@@ -29,6 +30,16 @@ router.post("/notes", (req, res) => {
       path.join(__dirname, "../db/db.json"),
       JSON.stringify(saveNote)
     );
+    res.status(200).send(saveNote);
+  });
+});
+
+router.delete("/notes/:id", (req, res) => {
+  const body = req.body;
+  fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
+    if (err) {
+      throw err;
+    }
   });
 });
 
